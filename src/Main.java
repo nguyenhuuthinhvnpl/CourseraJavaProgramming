53e7d2f5479bb06c32008fe8d971f735602b1f98
nguyenhuuthinhvnpl <nguyenhuuthinhvnpl@gmail.com>
2018-06-10 17:12:16 +0200

    Pulling version 1
    This is pulling version 1.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main( String[] args ) {
         System.out.println("This is the empty file!");
         System.out.println("This is the second version");
-        System.out.println("This is the pulling version");
+        System.out.println("This is the pulling version 1");
         
     }
 }
